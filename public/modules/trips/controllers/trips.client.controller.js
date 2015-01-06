841ac3c524aae078155ec5319d1c31fae6b2353e
vascofg <vascofg@gmail.com>
2015-01-06 02:30:16 +0000

    small bugfix

--- public/modules/trips/controllers/trips.client.controller.js
+++ public/modules/trips/controllers/trips.client.controller.js
@@ -179,8 +179,6 @@ angular.module('trips').controller('TripsController', ['$scope', '$stateParams',
         var marker = {
           place_name: place.name,
           place_id: place.place_id,
-          start: Date.now(),
-          end: Date.now(),
           location: {
             latitude: place.geometry.location.lat(),
             longitude: place.geometry.location.lng()
